c63b9e595ec91f186d27086d55955d6a706cb32f
Diego Vega <diego.vega.camacho@gmail.com>
2023-10-10 11:13:27 -0600

    Added Project to in order to Clone

---
+++ Examen_iOS/Examen_iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Examen_iOS
+//
+//  Created by Diego Vega Camacho on 10/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+//#Preview {
+//    ContentView()
+//}
---
+++ Examen_iOS/Examen_iOS/Examen_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Examen_iOSApp.swift
+//  Examen_iOS
+//
+//  Created by Diego Vega Camacho on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Examen_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
